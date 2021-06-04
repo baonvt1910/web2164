3aeb31a1e107eb9a4d81171efb51f50f4cc6a610
baonvt1910 <84694936+baonvt1910@users.noreply.github.com>
2021-06-04 10:34:02 +0700

    Add files via upload

--- 02.03.01_index.js
+++ 02.03.01_index.js
@@ -1,39 +1,43 @@
- //Tạo  Project và ghi nhận thông tin liên quan 
- // npm init --y 
- // npm install --save express ( để cài thư viện express)
- // Mở thư viện fs,express
- // const fs = require("fs");
- 
-  
- const express = require("express");
- // Tham số - Web lắng nghe tại xport
- xport = process.env.PORT || 3000;
- // Tạo App Web
- const app = express();
- const router = express.Router();
- // Routing WEB
+// Tạo Project và Ghi nhận thông tin liên quan
+// npm init --y
+// npm install --save express
+// Mở thư viện fs, express
+// const fs = require("fs");
+const express = require("express");
+var path = require('path');
+// Tham số - Web lắng nghe tại xport
+xport = process.env.PORT || 3000;
+// Tạo App Web
+const app = express();
+const router = express.Router();
+// Routing WEB
+// MVC
 
-
- //--- Home
+//--- Home
 router.get('/',
     (req, res) => {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.send("Đây là trang Home !");
+        res.sendFile(__dirname + "/view/home.html");
     }
 );
 
 //--- Profile
 router.get('/profile',
-(req, res) => {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.send("Đây là trang Profile !");
+    (req, res) => {
+        res.sendFile(__dirname + "/view/profile.html");
     }
 );
+
+//--- Login
+router.get('/login',
+    (req, res) => {
+        res.sendFile(__dirname + "/view/login.html");
+    }
+);
+
 //--- Logout
 router.get('/logout',
     (req, res) => {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.send("Đây là trang Logout !");
+        res.sendFile(__dirname + "/view/logout.html");
     }
 );
 
